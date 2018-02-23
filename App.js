f08c13f872524e31e950d4d15f10c7468eb70c70
Tommy Mallow <atomikm@gmail.com>
2018-02-22 17:43:25 -0800

    This better work

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {StyleSheet, Text, View, Modal } from 'react-native'
+import { StackNavigator } from 'react-navigation';
+import StartupScreen from './StartupScreen'
+import LoginScreen from './LoginScreen'
+import SignupScreen from './SignupScreen'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+const Navigation = StackNavigator({
+  Home: { screen: StartupScreen },
+  Login: { screen: LoginScreen },
+  Signup: { screen: SignupScreen },
 });
+
+export default Navigation;
---
+++ StartupScreen.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {StyleSheet, Text, View, Image, Modal, TextInput } from 'react-native'
+import RoundedButton from './App/Components/RoundedButton'
+import FullButton from './App/Components/FullButton'
+
+export default class StartupScreen extends React.Component {
+
+  render() {
+    var {navigate} = this.props.navigation;
+    return (
+
+
+      <View style={styles.container}>
+      <Image source={require('./App/Images/Logo.png')} style={{width:150, height: 193, marginBottom: 60}}/>
+      <TextInput
+                    style={styles.textInput}
+                    placeholder={'Username'}
+                    placeholderTextColor= '#ffffff'
+            />
+      <TextInput
+                    style={styles.textInput}
+                    placeholder={'Password'}
+                    placeholderTextColor= '#ffffff'
+                    secureTextEntry = {true}
+            />
+      <RoundedButton title="Go to login screen" onPress={() => navigate('Login', {})}>
+          Login
+      </RoundedButton>
+      <RoundedButton title="Go to signup screen" onPress={() => navigate('Signup', {})}>
+          Sign-Up
+      </RoundedButton>
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#84C9E0',
+  alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    borderRadius: 5, color: '#ffffff', width: 250, height: 40, backgroundColor: '#f0f0f0', margin: 10, marginRight: 25, marginLeft: 25,
+  }
+});
